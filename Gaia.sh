cefa7a55236c63a7cb1fd31329c0f829c94e0183
Harishpsy <127644923+Harishpsy@users.noreply.github.com>
2024-10-20 21:21:41 +0530

    Create Gaia.sh

---
+++ Gaia.sh
@@ -0,0 +1,7 @@
+echo "# Gaia" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Harishpsy/Gaia.git
+git push -u origin main
